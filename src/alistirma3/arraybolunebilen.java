b762f6d9a675eb327c6d25071e0a72e77d4c44f0
Halil Yılar <halil.yilarr@gmail.com>
2022-05-08 15:14:16 +0300

    array bolunebilen

---
+++ src/alistirma3/arraybolunebilen.java
@@ -0,0 +1,27 @@
+package alistirma3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arraybolunebilen {
+    public static void main(String[] args) {
+       // int[] array ={ 12, -11, 7, 23,34,42,67,98 };
+       int arr[]=new int[8];
+       int sayac=0;
+
+        for (int i = 0; i <arr.length; i++) {
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println( i + "elemanı  giriniz");
+            arr[i]=scan.nextInt();
+
+            if (i%3==0 ){
+                sayac++;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(sayac);
+
+    }
+}
